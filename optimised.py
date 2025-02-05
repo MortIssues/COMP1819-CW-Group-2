de9b94b776499b0d3193b38bc98374096902754b
MortIssues <leocmgirling@gmail.com>
2025-02-05 01:49:44 +0000

    Added 'optimised'
    File is functionally the same just with optimisations.

--- optimised.py
+++ optimised.py
@@ -13,7 +13,6 @@ def int_sub_string(string):
 
     return list(dict.fromkeys(result))
 
-
 def is_prime(num):
     """ Function to check if a number is a prime number.
     :param num: The number to be checked.
@@ -21,19 +20,17 @@ def is_prime(num):
     :rtype: bool
     """
 
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, num ** 0.5):
         if num % i == 0:
             return False
     return True
 
+def sub_string_primes(string, maximum):
+    sub_strings = int_sub_string(string)
+    return list(filter(is_prime, filter(lambda x: x < maximum, sub_strings) if maximum > 0 else sub_strings))
+
 
 if __name__ == "__main__":
-    bin_string, max = input("Enter a binary number and the maximum prime to find: ").split()
+    bin_string, maximum = input("Enter a binary number and the maximum prime to find (0 for no limit): ").split()
 
-    result = []
-
-    for num in int_sub_string(bin_string):
-        if is_prime(num) and num < int(max):
-            result.append(num)
-
-    print(result)
+    print(sub_string_primes(bin_string, int(maximum)))
